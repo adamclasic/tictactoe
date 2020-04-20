a6580a118ad3344e78e061654a90b684048f3742
phillip musiime <phillipmusiime@gmail.com>
2020-04-20 18:18:33 +0300

    Added get player info method

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,27 @@
 #!/usr/bin/env ruby
 
-def get_player_info(num)
+
+ def get_player_info
+    p 'enter name of player 1'
+	player_1 = gets.chomp
+	p "enter letter"
+	player_1_letter = gets.chomp
+	p "#{player_1} chose #{player_1_letter}"
+	 p 'enter name of player 2'
+	player_2 = gets.chomp
+	p "enter letter"
+	player_2_letter = gets.chomp
+	p "#{player_2} chose #{player_2_letter}"
+end
+
+
+
+# get_player_info
+
+
+
+
+def get_player_num(num)
     p "enter Player #{num} name"
     player = gets.chomp
 end
@@ -39,13 +60,13 @@ player_2 = get_player_info(2)
 
 
 
-x1 = ['x', 'o', 'x']
+# x1 = ['x', 'o', 'x']
 
-p "      1   2   3  "    
-p "    -------------"    
-p "  A | #{x1[1]} | #{x1[2]} | #{x1[0]} |"
-p "    -------------"
-p "  B | #{x1[1]} | #{x1[2]} | #{x1[0]} |"
-p "    -------------"
-p "  C | #{x1[1]} | #{x1[2]} | #{x1[0]} |"
-p "    -------------"
+# p "      1   2   3  "    
+# p "    -------------"    
+# p "  A | #{x1[1]} | #{x1[2]} | #{x1[0]} |"
+# p "    -------------"
+# p "  B | #{x1[1]} | #{x1[2]} | #{x1[0]} |"
+# p "    -------------"
+# p "  C | #{x1[1]} | #{x1[2]} | #{x1[0]} |"
+# p "    -------------"
