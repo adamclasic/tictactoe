b77d1a2ee2d2a2fd8aa7a4b38c52727373420a4e
adam <aabou9@gmail.com>
2020-04-21 11:38:37 +0100

    update rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,33 +1,32 @@
 #!/usr/bin/env ruby
 
 p 'enter name of player 1 or press Enter to skip'
-input_1 = gets.chomp
-input_1 == '' ? 'player 1' : input_1
+input1 = gets.chomp
+input1 == '' ? 'player 1' : input1
 p 'enter a letter to play with'
 player_1_letter = gets.chomp
-p "#{input_1} chose #{player_1_letter}"
+p "#{input1} chose #{player_1_letter}"
 
 p 'enter name of player 2 or press Enter to skip'
-input_2 = gets.chomp
-input_2 == '' ? 'player 2' : input_2
+input2 = gets.chomp
+input2 == '' ? 'player 2' : input2
 player_2_letter = 'O' if player_1_letter == 'X'
 player_2_letter = 'X' if player_1_letter == 'O'
-p "#{input_2} will be playin with #{player_2_letter}"
+p "#{input2} will be playin with #{player_2_letter}"
 
 sleep(1.5)
 puts ''
 
-p "The game will start ..."
+p 'The game will start ...'
 
 puts ''
 sleep(2)
 puts ''
 puts ''
 
-
 10.times do |i|
-  player = input_1 if i.even?
-  player = input_2 if i.odd?
+  player = input1 if i.even?
+  player = input2 if i.odd?
 
   p "#{player} enter Horizontal position  with a letter "
   gets.chomp
