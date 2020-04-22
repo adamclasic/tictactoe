bd4b8b00af9b641dae397f355d8c8917e4d674c1
phillipug <phillipmusiime@gmail.com>
2020-04-22 15:08:38 +0300

    added starting instructions and terminating variables

--- bin/main.rb
+++ bin/main.rb
@@ -1,37 +1,53 @@
 #!/usr/bin/env ruby
 
-p 'enter name of player 1 or press Enter to skip'
+# user instructions
+puts 'Welcome to **Tic-Tac-toe**'
+puts 'In this game whichever player gets the first similar letters in a row, column or diagonal Wins!.'
+puts "There are only two letters to pick from 'X' and 'O'."
+puts 'At the begining of the game, each player will choose which letter they will be playing with,'
+puts 'once one player has selected a letter, the otherone is assigned the other letter automatically.'
+puts "Note: Player names are optional (just press 'Enter' to continue)"
+puts ''
+puts ''
+
+#Get user input and set it to a variable
+p 'Enter name of player 1 or press Enter to skip'
 input1 = gets.chomp
 input1 == '' ? 'player 1' : input1
 p 'enter a letter to play with'
 player_1_letter = gets.chomp
 p "#{input1} chose #{player_1_letter}"
 
-p 'enter name of player 2 or press Enter to skip'
+p 'Enter name of player 2 or press Enter to skip'
 input2 = gets.chomp
 input2 == '' ? 'player 2' : input2
 player_2_letter = 'O' if player_1_letter == 'X'
 player_2_letter = 'X' if player_1_letter == 'O'
 p "#{input2} will be playin with #{player_2_letter}"
 
-sleep(1.5)
 puts ''
 
 p 'The game will start ...'
 
 puts ''
-sleep(2)
-puts ''
-puts ''
 
-10.times do |i|
-  player = input1 if i.even?
-  player = input2 if i.odd?
+game_on = true
+
+player = input1 # if i.even?
+# player = input2 if i.odd?
+
+while game_on #Loop for each move
 
   p "#{player} enter Horizontal position  with a letter "
   gets.chomp
   p "#{player} enter Vertical position  with a number "
   gets.chomp
+
+  valid = true
+  unless valid #checks if input from user is valid
+    puts "Error, input invalid. Please enter 'X' or 'O'"
+    next
+  end
 
   x1 = [%w[x o x], %w[x o x], %w[x o x]]
 
@@ -43,4 +59,14 @@ puts ''
   p '    -------------'
   p "  C | #{x1[2][1]} | #{x1[2][1]} | #{x1[2][2]} |"
   p '    -------------'
+
+  draw = false
+  if draw #Condition for when game is draw
+    puts 'this is a Draw'
+    game_on = false
+  end
+
+  winner = true
+  game_on = false if winner #terminates when we have a winner
+
 end
