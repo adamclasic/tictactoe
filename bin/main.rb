b5831085e608ed00dff011c48aa5759d28d3dcca
adam <aabou9@gmail.com>
2020-04-23 15:51:17 +0100

    update rubocop

--- bin/main.rb
+++ bin/main.rb
@@ -1,97 +1,86 @@
 #!/usr/bin/env ruby
 require_relative './tictactoe.rb'
 
+puts
 puts 'Welcome to **Tic-Tac-toe**'
-puts 'In this game whichever player gets the first similar letters in a row, column or diagonal Wins!.'
-puts "There are only two letters to pick from 'X' and 'O'."
-puts 'At the begining of the game, each player will choose which letter they will be playing with,'
-puts 'once one player has selected a letter, the otherone is assigned the other letter automatically.'
-puts "Note: Player names are optional (just press 'Enter' to continue)"
-puts ''
-puts ''
+Tictactoe.print_table_empty
+puts "In this game, there are only two letters to play with 'X' and 'O'. Whichever player gets the first similar letters in an entire row, column or diagonal WINS!!!."
+puts
+puts "At the begining of the game, player 1 will choose either 'X' or 'O' and the other letter will be assigned to player 2 automatically."
+puts
 
-p 'Enter name of player 1 or press Enter to skip'
+puts 'Enter name of player 1 or press Enter to skip'
 input1 = gets.chomp
 input1 == '' ? input1 = 'player 1' : input1
 
-p 'enter a letter to play with'
+puts 'enter a letter to play with'
 player_1_letter = gets.chomp.upcase
-unless player_1_letter == 'X' || player_1_letter == 'O' 
+until %w[X O].include?(player_1_letter)
   puts 'enter a valid letter to play with'
-  until player_1_letter == 'X' || player_1_letter == 'O'
-    letter = gets.chomp.upcase
-    if Tictactoe.valid_letter?(letter)
-      player_1_letter = letter
-    else
-      puts 'enter a valid letter to play with'
-    end
-  end
+  letter = gets.chomp.upcase
+  player_1_letter = letter if Tictactoe.valid_letter?(letter)
 end
 
-p "#{input1} chose #{player_1_letter}"
+puts "#{input1} chose #{player_1_letter}"
 
-p 'Enter name of player 2 or press Enter to skip'
+puts 'Enter name of player 2 or press Enter to skip'
 input2 = gets.chomp
 input2 == '' ? input2 = 'player 2' : input2
 player_2_letter = 'O' if player_1_letter == 'X'
 player_2_letter = 'X' if player_1_letter == 'O'
-p "#{input2} will be playin with #{player_2_letter}"
+puts "#{input2} will be playin with #{player_2_letter}"
 
 puts ''
 
-p 'The game will start ...'
+puts 'The game will start ...'
 
 puts ''
 
 game_on = true
 
-game = Tictactoe.new(player_1_letter, player_2_letter, [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
-p '======='
-p game
-p '======='
+game = Tictactoe.new([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
+
+Tictactoe.draw_table
+
 i = 0
-while game_on 
+while game_on
   player_letter = player_1_letter if i.even?
   player_name = input1 if i.even?
   player_letter = player_2_letter if i.odd?
   player_name = input2 if i.odd?
-  
-  
-  
-  p "#{player_name} enter a position from 1 to 9 "
+
+  puts "#{player_name} enter a position from 1 to 9 "
   location = gets.chomp.to_i - 1
   valid = game.valid_entry?(location)
-  unless valid 
+  unless valid
     until valid
-    puts "Error, input invalid. #{player_name} Please re enter"
-    location = gets.chomp.to_i - 1
-    valid = game.valid_entry?(location)
+      puts "Error, input invalid. #{player_name} Please re enter"
+      location = gets.chomp.to_i - 1
+      valid = game.valid_entry?(location)
     end
   end
-  
-  
-  
+
   table = game.add_to_table(player_letter, location)
-  
-  p '      1   2   3  '
-  p '    -------------'
-  p "  1 | #{table[0]} | #{table[1]} | #{table[2]} |"
-  p '    -------------'
-  p "  4 | #{table[3]} | #{table[4]} | #{table[5]} |"
-  p '    -------------'
-  p "  7 | #{table[6]} | #{table[7]} | #{table[8]} |"
-  p '    -------------'
-  
-  
-  winner = Code.is_winner(table, player_letter)
-  
+
+  puts '      1   2   3  '
+  puts '    -------------'
+  puts "  1 | #{table[0]} | #{table[1]} | #{table[2]} | 3"
+  puts '    -------------'
+  puts "  4 | #{table[3]} | #{table[4]} | #{table[5]} | 6"
+  puts '    -------------'
+  puts "  7 | #{table[6]} | #{table[7]} | #{table[8]} | 9"
+  puts '    -------------'
+  puts '      7   8   9  '
+
+  winner = Code.winner?(table, player_letter)
+
   if winner
     puts "#{player_name} has won the game!"
     game_on = false
   end
-  
+
   draw = Code.draw?(table)
-  if draw 
+  if draw && !winner
     puts 'this is a Draw'
     game_on = false
   end
--- bin/tictactoe.rb
+++ bin/tictactoe.rb
@@ -1,42 +1,46 @@
 require_relative './code.rb'
 class Tictactoe
-    # def table
-    #     @table = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    # end
+  def initialize(arr)
+    @table = arr
+  end
 
-    def initialize(player1, player2, arr)
-        @player1 = player1
-        @player2 = player2
-        @table = arr
-    end
+  def self.valid_letter?(letter)
+    return true if %w[X O].include?(letter)
+  end
 
-    def self.valid_letter?(letter)
-        return true if letter == 'O' || letter == 'X'
-    end
+  def valid_entry?(location)
+    return true if @table[location] == ' '
 
-    def valid_entry?(location)
-        if @table[location] == ' '
-            return true
-        else
-            return false
-        end
-    end
+    false
+  end
 
-    def add_to_table(player, position)
-        if valid_entry?(position)
-            @table[position] = player
-            @table
-        else
-            raise 'invalid entry can not add player to the requested position'
-        end
-    end
-    # attr_reader :table 
+  def add_to_table(player, position)
+    # unless valid_entry?(position) raise 'invalid entry can not add player to the requested position'
+    @table[position] = player
+    @table
+  end
 
+  def self.print_table_empty
+    puts '      1   2   3  '
+    puts '    -------------'
+    puts '  1 | X | O | X | 3'
+    puts '    -------------'
+    puts '  4 | O | X | O | 6'
+    puts '    -------------'
+    puts '  7 | X | O | X | 9'
+    puts '    -------------'
+    puts '      7   8   9  '
+  end
+
+  def self.draw_table
+    puts '      1   2   3  '
+    puts '    -------------'
+    puts '  1 |   |   |   | 3'
+    puts '    -------------'
+    puts '  4 |   |   |   | 6'
+    puts '    -------------'
+    puts '  7 |   |   |   | 9'
+    puts '    -------------'
+    puts '      7   8   9  '
+  end
 end
-
-# game = Tictactoe.new('x', 'o')
-# p game.table
-# p game.valid_entry?(1)
-# p game.add_to_table('x', 0)
-# p game.add_to_table('x', 3)
-# p game.add_to_table('o', 7)
